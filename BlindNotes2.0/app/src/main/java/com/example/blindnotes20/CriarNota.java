dda7f80b78095345a278156e8d73ce9c98206c36
Danielammb <dmedeirosbatista@gmail.com>
2021-05-14 11:35:45 -0300

    Blind notes refresh

---
+++ BlindNotes2.0/app/src/main/java/com/example/blindnotes20/CriarNota.java
@@ -0,0 +1,14 @@
+package com.example.blindnotes20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CriarNota extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criar_nota);
+    }
+}
