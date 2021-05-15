8cce9ff6b88b1b4768c8a87a0c07536a51d20709
Danielammb <dmedeirosbatista@gmail.com>
2021-05-15 12:35:44 -0300

    Dev navigations

--- BlindNotes2.0/app/src/main/java/com/example/blindnotes20/CriarNota.java
+++ BlindNotes2.0/app/src/main/java/com/example/blindnotes20/CriarNota.java
@@ -2,7 +2,9 @@ package com.example.blindnotes20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CriarNota extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class CriarNota extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criar_nota);
+    }
+    public void irParaMain(View view){
+        Intent intent1 = new Intent(this, MainActivity.class);
+        startActivity(intent1);
     }
 }
